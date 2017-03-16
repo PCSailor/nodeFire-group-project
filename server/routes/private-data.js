1fa86cb3fb709f3fdabb663359bba545b23af241
Phil Curtis <Phil@PhilCurtis.io>
2017-03-16 13:26:04 -0500

    working into db and onto DOM

--- server/routes/private-data.js
+++ server/routes/private-data.js
@@ -23,7 +23,6 @@ router.get("/", function(req, res){
       } else {
         // Based on the clearance level of the individual, give them access to different information
         Secret.find({ secrecyLevel: { $lte: user.clearanceLevel } }, function (err, secrets){ // NOTE:  Mongoose callback function
-
           if (err) {
             console.log('Error COMPLETING secrecyLevel query task', err);
             res.sendStatus(500);
@@ -39,75 +38,18 @@ router.get("/", function(req, res){
 
 // post to db
 router.post("/", function(req, res){
-  // var userEmail = req.decodedToken.email;
-  // console.log(userEmail);
   console.log(req.body);
-
-
-
-  // var secretObject = req.body;
-  // var addedSecret = new Secret({
-  //   secretText: secretObject.secretText,
-  //   secrecyLevel: secretObject.secrecyLevel
-  // });
-
-  // Check the user's level of permision based on their email
-  // User.findOne({ email: userEmail }, function (err, user) {
-
-  // Secret.insert({ addedSecret }, function (err, secret) {
-  // // Secret.insert( addedSecret, function (err, secret) {
-  // Secret.save({ addedSecret}, function (err, secret) {
-
-
-
   var addedSecret = new Secret(req.body);
-   console.log(addedSecret);
-   addedSecret.save(function(err, data) {
-     console.log('save data:', data);
-     if(err) {
-       console.log('ERR: ', err);
-       res.sendStatus(500);
-     } else {
-       res.sendStatus(201);
-     }
-   });
-
-
-
-
-
-    //   res.sendStatus(500);
-    // } else {
-    //   console.log(user);
-    //   if(user == null) {
-    //     // If the user is not in the database, return a forbidden error status
-    //     console.log('No user found with that email. Have you added this person to the database? Email: ', req.decodedToken.email);
-    //     res.sendStatus(403);
-
-
-
-      // } else {
-      //   var secretObject = req.body;
-      //   var addedSecret = new Secret({
-      //     secretText: secretObject.secretText,
-      //     secrecyLevel: secretObject.secrecyLevel
-      //   });
-
-
-
-  //db query
-        // addedSecret.save(function(err, result){
-        //   if(err){
-        //     console.log('error on post', err);
-        //     res.sendStatus(500);
-        //   } else {
-        //     // return all of the results where a specific user has permission
-        //     res.send(result);
-        //   }
-        // });
-      // }
-    // }
-  // });
+  console.log(addedSecret);
+  addedSecret.save(function(err, data) {
+    console.log('save data:', data);
+    if(err) {
+      console.log('ERR: ', err);
+      res.sendStatus(500);
+    } else {
+      res.sendStatus(201);
+    }
+  });
 });
 
 module.exports = router;
